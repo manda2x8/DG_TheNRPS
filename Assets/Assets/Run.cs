91ed7273b9da736bb7a979aa2c9390d15e19d981
Milan <m2001.mandic8@hotmail.com>
2023-05-14 10:51:48 +0200

    3nd commit

---
+++ Assets/Assets/Run.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Run : MonoBehaviour
+{
+    public void FirstScene(){
+        SceneManager.LoadScene("New Scene");
+    }
+}
